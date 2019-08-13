3ea57c527e8aaa95be4532d90004d9279e5e8783
Paul Wright <pwright@redhat.com>
2019-08-13 15:53:32 +0100

    update

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -2,20 +2,20 @@
 echo $SHELL
  
 echo 'publish-adoc'
-gem install asciidoctor
+# gem install asciidoctor
 
 
-for filename in ../*/master.adoc; do
+for filename in ./*/master.adoc; do
 
-   echo $filename
+  echo $filename
   asciidoctor -b html5  -a toc=left -a theme=flask -a include-tags="!excludeDownstream;!excludeAll" $filename 
 
 done
 
 
-for filename in ../*/master.html; do
+for filename in ./*/master.html; do
 
-   echo $filename
+  echo $filename
   sed -i '/Table of Contents/c\<a href=../index.html>Home</a><br><br>' $filename
 
 done
